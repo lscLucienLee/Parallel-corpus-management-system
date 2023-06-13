518d53fc6fefc6b3adddca3189a0c3a634db65bb
djkun4864 <1302554864@qq.com>
2023-06-13 15:20:15 +0800

    dj

--- Code/PCMS/app/static/js/browse1.js
+++ Code/PCMS/app/static/js/browse1.js
@@ -1,27 +1,24 @@
 
     var table = document.querySelector("table")
+    //模拟后台响应的数据
     $(document).ready(function () {
-    let tbody = document.querySelector("tbody");
-    $.ajax({
-        url: '/path/to/resource',
-        type: 'GET',
-        dataType: 'text',
-        success: function (data) {
-            console.log(data);
+        $.ajax({
+            url: '/browse/data/',
+            type: 'GET',
+            dataType: 'text',
+            success: function (data) {
+                console.log(data);
+                // 处理响应数据
+                let json = JSON.parse(data);
+                var ul = document.querySelector(".pagination");
+        var page_number = 3; //单页浏览的条数
+        var Total_pages; //页数
+        var liAll; //页码按钮下标为 1到length-2是页数 0和length-1为上一页和下一页
+        var pre; //上一页
+        var next; //下一页
 
-
-            let json = JSON.parse(data);
-
-
-    var ul = document.querySelector(".pagination");
-    var page_number = 10;
-    var Total_pages;
-    var liAll;
-    var pre;
-    var next;
-
-    function clearTable() {
-        table.innerHTML = `
+        function clearTable() {
+            table.innerHTML = `
         <tbody>
              <tr>
                  <th style="width: 50px">#</th>
@@ -31,9 +28,9 @@
             </tr>
         </tbody>
         `
-    }
-    window.onload = function() {
-        json.forEach(function(item, i) {
+        }
+
+        json.forEach(function (item, i) {
             var tbody = document.querySelector("tbody");
             if (i < page_number) {
                 var tr = document.createElement("tr");
@@ -51,9 +48,9 @@
 
         })
 
-        var len = json.length; //?????
+        var len = json.length; //总记录条数
 
-        Total_pages = len % page_number == 0 ? len / page_number : len / page_number + 1; //??
+        Total_pages = len % page_number == 0 ? len / page_number : len / page_number + 1; //页数
 
         for (var i = 1; i <= Total_pages; i++) {
             ul.innerHTML += `
@@ -71,21 +68,21 @@
         liAll = document.querySelectorAll("li");
 
         // console.log([liAll])
-        var pagethis = 1; //??????
+        var pagethis = 1; //当前是第几页
         for (var i = 1; i < liAll.length - 1; i++) {
-            liAll[i].onclick = function() {
+            liAll[i].onclick = function () {
                 for (var j = 1; j < liAll.length - 1; j++) {
 
                 }
-                pagethis = this.id; //????????
+                pagethis = this.id; //获取当前是第几页
 
                 // console.log(liAll[i])
-                let start; //?????????
-                let end; //?????????
+                let start; //当页数据的起始下标
+                let end; //当页数据的结束下标
                 if (pagethis != 1) {
                     start = (pagethis - 1) * page_number;
-                    end = start + page_number;
-                    if (end > json.length - 1) { //??????????????????????????????????
+                    end = start + page_number - 1;
+                    if (end > json.length - 1) { //如果当页数据结束值大于总数据条数下标的值则赋值为总数据条数最大下标值
                         end = json.length - 1;
                     }
                 } else {
@@ -95,8 +92,51 @@
 
                 clearTable();
                 var tbody = document.querySelector("tbody");
-                json.forEach(function(item, i) {
+                json.forEach(function (item, i) {
 
+                    if (i >= start && i <= end) {
+                        var tr = document.createElement("tr");
+                        tr.innerHTML = `
+                           <td style="width: 50px">${item.id}</td>
+                            <td style="width: 400px">${item.original}</td>
+                            <td style="width: 400px">${item.translation}</td>
+                            <td style="width:140px">
+              <a href="/update/"><button type="button" class="btn btn-default btn-sm" style="width:50px;height:25px;background-color:#DCDCDC">??</button></a>
+              <button type="button" class="btn btn-default btn-sm" style="width:50px;height:25px;background-color:#DCDCDC" onclick="openwindow()">??</button>
+          </td>
+                            `
+                        tbody.appendChild(tr);
+                    }
+                })
+
+            }
+        }
+        pre = document.querySelector("#pre") //上一页
+        next = document.querySelector("#next") //下一页
+        pre.onclick = function () {
+
+            if (pagethis != 1) { //当前页数不等于1时执行上一页
+                pagethis--;
+                for (var j = 1; j < liAll.length - 1; j++) {
+
+                }
+
+                let start;
+                let end;
+                if (pagethis != 1) {
+                    start = (pagethis - 1) * page_number;
+                    end = start + page_number - 1;
+                    if (end > json.length - 1) {
+                        end = json.length - 1;
+                    }
+                } else {
+                    start = 0;
+                    end = page_number - 1;
+                }
+
+                clearTable();
+                var tbody = document.querySelector("tbody");
+                json.forEach(function (item, i) {
                     if (i >= start && i <= end) {
                         var tr = document.createElement("tr");
                         tr.innerHTML = `
@@ -108,18 +148,17 @@
               <button type="button" class="btn btn-default btn-sm" style="width:50px;height:25px;background-color:#DCDCDC" onclick="openwindow()">??</button>
           </td>
                             `
+                        console.log(tr)
                         tbody.appendChild(tr);
                     }
                 })
-
             }
         }
-        pre = document.querySelector("#pre") //???
-        next = document.querySelector("#next") //???
-        pre.onclick = function() {
 
-            if (pagethis != 1) { //???????1??????
-                pagethis--;
+        next.onclick = function () {
+            // alert(pagethis)
+            if (pagethis < liAll.length - 2) { //当前页数小于最后一页则执行下一页
+                pagethis++;
                 for (var j = 1; j < liAll.length - 1; j++) {
 
                 }
@@ -128,7 +167,7 @@
                 let end;
                 if (pagethis != 1) {
                     start = (pagethis - 1) * page_number;
-                    end = start + page_number;
+                    end = start + page_number - 1;
                     if (end > json.length - 1) {
                         end = json.length - 1;
                     }
@@ -139,11 +178,11 @@
 
                 clearTable();
                 var tbody = document.querySelector("tbody");
-                json.forEach(function(item, i) {
+                json.forEach(function (item, i) {
                     if (i >= start && i <= end) {
                         var tr = document.createElement("tr");
                         tr.innerHTML = `
-                             <td style="width: 50px">${item.id}</td>
+                            <td style="width: 50px">${item.id}</td>
                             <td style="width: 400px">${item.original}</td>
                             <td style="width: 400px">${item.translation}</td>
                             <td style="width:140px">
@@ -157,50 +196,15 @@
                 })
             }
         }
-        next.onclick = function() {
-            // alert(pagethis)
-            if (pagethis < liAll.length - 2) { //????????????????
-                pagethis++;
-                for (var j = 1; j < liAll.length - 1; j++) {
-
-                }
-
-                let start;
-                let end;
-                if (pagethis != 1) {
-                    start = (pagethis - 1) * page_number;
-                    end = start + page_number;
-                    if (end > json.length - 1) {
-                        end = json.length - 1;
-                    }
-                } else {
-                    start = 0;
-                    end = page_number - 1;
-                }
-
-                clearTable();
-                var tbody = document.querySelector("tbody");
-                json.forEach(function(item, i) {
-                    if (i >= start && i <= end) {
-                        var tr = document.createElement("tr");
-                        tr.innerHTML = `
-                             <td style="width: 50px">${item.id}</td>
-                            <td style="width: 400px">${item.original}</td>
-                            <td style="width: 400px">${item.translation}</td>
-                            <td style="width:140px">
-             <a href="/update/"><button type="button" class="btn btn-default btn-sm" style="width:50px;height:25px;background-color:#DCDCDC">??</button></a>
-              <button type="button" class="btn btn-default btn-sm" style="width:50px;height:25px;background-color:#DCDCDC" onclick="openwindow()">??</button>
-          </td>
-                            `
-                        console.log(tr)
-                        tbody.appendChild(tr);
-                     }
-                })
-            }
-        }
             }
         })
 
 
     })
+
+
+
+
+
+
 
