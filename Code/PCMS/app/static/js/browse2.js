a957fe6bbba8a8784d49e06e6ed65b15b49f3819
djkun4864 <1302554864@qq.com>
2023-06-13 14:48:10 +0800

    dj

--- Code/PCMS/app/static/js/browse2.js
+++ Code/PCMS/app/static/js/browse2.js
@@ -6,3 +6,32 @@ function openwindow(){
 
 		}
 	}
+function search(){
+    let wordVal = $('input[name="word"]').val();
+    if (wordVal === " ") {
+        alert("请输入查询内容！");
+    } else {
+        $("#word").text(wordVal);
+        let requestBody = {
+            word: 'word'
+        };
+        requestBody.word = wordVal;
+        $.ajax({
+            url: '/path/to/resource',
+            type: 'POST',
+            data: requestBody,
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                console.log(data.frequencyId);
+                $("#frequencyId").text(data.frequencyId);
+                // 处理响应数据
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Error: " + errorThrown);
+                // 处理错误
+            }
+        });
+
+    }
+}
