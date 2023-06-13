8f3e27eb8d1bbae49d209f20258bc95ddfe0a2df
VielotYZH <1115494135@qq.com>
2023-06-13 10:42:30 +0800

    yzh 手动创建语料库

--- Code/PCMS/app/views/Delete_corpuses.py
+++ Code/PCMS/app/views/Delete_corpuses.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from Code.PCMS.app import models
+from Code.PCMS.app import corpusmanager
 
 
 def delete_corpuses(request):
     id = request.GET.get('id')
+    file_to_delete=models.CorpusInfo.objects.filter(id=id).first().name+".csv"
     models.CorpusInfo.objects.filter(id=id).delete()
+    print(file_to_delete)
+    manager = corpusmanager.ParallelCorpusManager()
+    manager.delete_corpus(file_to_delete)
     return redirect("/manage/")
