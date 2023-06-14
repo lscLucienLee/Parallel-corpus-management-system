a182b84a589f0f7acff25b1f185035e0baff4b66
djkun4864 <1302554864@qq.com>
2023-06-14 16:27:51 +0800

    dj update

--- Code/PCMS/app/static/js/merge.js
+++ Code/PCMS/app/static/js/merge.js
@@ -1,25 +1,32 @@
+/*
+方法:get
+res
+[{
+    "id": "1001",
+    "original": "java核心技术1",
+    "standard_translation": "120",
+    "machine_translation": "res/productimg/1.jpeg"
+},{
+...
+}
+]
+*/
 
-    var table = document.querySelector("table")
-    //模拟后台响应的数据
-    $(document).ready(function () {
-        $.ajax({
-            url: '/manage/data/',
-            type: 'GET',
-            dataType: 'text',
-            success: function (data) {
-                console.log(data);
-                // 处理响应数据
-                let json = JSON.parse(data);
-                var ul = document.querySelector(".pagination");
-        var page_number = 5; //单页浏览的条数
-        var Total_pages; //页数
-        var liAll; //页码按钮下标为 1到length-2是页数 0和length-1为上一页和下一页
-        var pre; //上一页
-        var next; //下一页
+let table = document.querySelector("table")
+//模拟后台响应的数据
 
-        function clearTable() {
-            table.innerHTML = `
-        <tbody>
+let ul = document.querySelector(".pagination");
+let page_number = 5; //单页浏览的条数
+let Total_pages; //页数
+let liAll; //页码按钮下标为 1到length-2是页数 0和length-1为上一页和下一页
+let pre; //上一页
+let next; //下一页
+let pageThis//当前页面号
+let start; //当页数据的起始下标
+let end; //当页数据的结束下标
+function clearTable() {
+    table.innerHTML = `
+            <tbody>
              <tr>
                 <th style="width:40px">#</th>
           <th style="width:320px;text-align: center">语料库名</th>
@@ -28,192 +35,265 @@
             </tr>
         </tbody>
         `
-        }
+}
 
-        json.forEach(function (item, i) {
-            var tbody = document.querySelector("tbody");
-            if (i < page_number) {
-                var tr = document.createElement("tr");
-                tr.innerHTML = `
-                             <td>${item.id}</td>
+
+window.onload = function () {
+         $.ajax({
+        url: '/merge/data/',
+        type: 'GET',
+        dataType: 'text',
+        success: function (data) {
+            console.log(data);
+            // 处理响应数据
+            let json = JSON.parse(data);
+
+            json.forEach(function (item, i) {
+                item.checked = false;
+            })
+            json.forEach(function (item, i) {
+                let tbody = document.querySelector("tbody");
+                if (i < page_number) {
+                    let tr = document.createElement("tr");
+                    tr.innerHTML = `
+                            <td>${item.nid}</td>
                             <td>${item.name}</td>
                             <td>${item.time}</td>
-                            <td><input id="${item.id}" name="${item.id}"  type="checkbox" style="margin-left: 40px"></td>
+                            <td><input id="${item.nid}" name="${item.nid}"  type="checkbox" style="margin-left: 40px"></td>
                             `
-                tbody.appendChild(tr);
-            }
-
-        })
-
-        var len = json.length; //总记录条数
-
-        Total_pages = len % page_number == 0 ? len / page_number : len / page_number + 1; //页数
-
-        for (var i = 1; i <= Total_pages; i++) {
-            ul.innerHTML += `
-            <li  id="${i}"><a href="#">${i}</a></li>
+                    tbody.appendChild(tr);
+                }
+            })
+            let len = json.length; //总记录条数
+            Total_pages = len % page_number === 0 ? len / page_number : len / page_number + 1; //页数
+            for (let i = 1; i <= Total_pages; i++) {
+                ul.innerHTML += `
+            <li class="page_li" id="${i}"><a href="#">${i}</a></li>
             `
-        }
-
-        ul.innerHTML += `
-                <li id="next">
+            }
+            ul.innerHTML += `
+                <li id="next" class="page_li">
                     <a href="#" aria-label="Next">
                         <span aria-hidden="true">&raquo;</span>
                     </a>
                 </li>
-        `;
-        liAll = document.querySelectorAll("li");
-
-        // console.log([liAll])
-        var pagethis = 1; //当前是第几页
-        for (var i = 1; i < liAll.length - 1; i++) {
-            liAll[i].onclick = function () {
-                for (var j = 1; j < liAll.length - 1; j++) {
-
-                }
-                pagethis = this.id; //获取当前是第几页
-
-                // console.log(liAll[i])
-                let start; //当页数据的起始下标
-                let end; //当页数据的结束下标
-                if (pagethis != 1) {
-                    start = (pagethis - 1) * page_number;
-                    end = start + page_number - 1;
-                    if (end > json.length - 1) { //如果当页数据结束值大于总数据条数下标的值则赋值为总数据条数最大下标值
-                        end = json.length - 1;
+            `;
+            liAll = document.querySelectorAll("li.page_li");
+            pageThis = 1; //当前是第几页
+            for (let i = 1; i < liAll.length - 1; i++) {
+                liAll[i].onclick = function () {
+                    console.log("上一页："+pageThis);
+                    if (pageThis !== 1) {
+                        start = (pageThis - 1) * page_number;
+                        end = start + page_number - 1;
+                        if (end > json.length - 1) { //如果当页数据结束值大于总数据条数下标的值则赋值为总数据条数最大下标值
+                            end = json.length - 1;
+                        }
+                    } else {
+                        start = 0;
+                        end = page_number - 1;
                     }
-                } else {
-                    start = 0;
-                    end = page_number - 1;
-                }
-                 json.forEach(function (item, i) {
+                    json.forEach(function (item, i) {
                         if (i >= start && i <= end) {
-
-                            item.checked = document.getElementById(item.id).checked;
+                            item.checked = document.getElementById(item.nid).checked;
                         }
                     })
                     // for (let j = 1; j < liAll.length - 1; j++) {
                     // }
+                    pageThis = this.id; //获取当前是第几页
+                    console.log("当前页："+pageThis);
 
+                    if (pageThis !== 1) {
+                        start = (pageThis - 1) * page_number;
+                        end = start + page_number - 1;
+                        if (end > json.length - 1) { //如果当页数据结束值大于总数据条数下标的值则赋值为总数据条数最大下标值
+                            end = json.length - 1;
+                        }
+                    } else {
+                        start = 0;
+                        end = page_number - 1;
 
-                clearTable();
-                var tbody = document.querySelector("tbody");
-                json.forEach(function (item, i) {
+                    }
 
-                    if (i >= start && i <= end) {
-                        var tr = document.createElement("tr");
-                        tr.innerHTML = `
-                        <td>${item.id}</td>
+                    clearTable();
+                    let tbody = document.querySelector("tbody");
+                    json.forEach(function (item, i) {
+
+                        if (i >= start && i <= end) {
+                            let tr = document.createElement("tr");
+                            if(item.checked===true){
+                                tr.innerHTML = `
+                            <td>${item.nid}</td>
                             <td>${item.name}</td>
                             <td>${item.time}</td>
-                            <td><input id="${item.id}" name="${item.id}"  type="checkbox" style="margin-left: 40px"></td>
+                            <td><input id="${item.nid}" name="${item.nid}"  type="checkbox" checked="${item.checked}" style="margin-left: 40px"></td>
                             `
-                        tbody.appendChild(tr);
-                    }
-                })
-
-            }
-        }
-        pre = document.querySelector("#pre") //上一页
-        next = document.querySelector("#next") //下一页
-        pre.onclick = function () {
-
-            if (pagethis != 1) { //当前页数不等于1时执行上一页
-                pagethis--;
-                for (var j = 1; j < liAll.length - 1; j++) {
-
+                            }else {
+                                tr.innerHTML = `
+                            <td>${item.nid}</td>
+                            <td>${item.name}</td>
+                            <td>${item.time}</td>
+                            <td><input id="${item.nid}" name="${item.nid}"  type="checkbox" style="margin-left: 40px"></td>
+                            `
+                            }
+                            tbody.appendChild(tr);
+                            console.log(item.checked);
+                        }
+                    })
                 }
-
-                let start;
-                let end;
-                if (pagethis != 1) {
-                    start = (pagethis - 1) * page_number;
+            }
+            pre = document.querySelector("#pre")
+            next = document.querySelector("#next") //下一页
+            pre.onclick = function () {
+                console.log(pageThis);
+                if (pageThis > 1) { //当前页数不等于1时执行上一页
+                    start = (pageThis - 1) * page_number;
+                    end = start + page_number - 1;
+                    if (end > json.length - 1) { //如果当页数据结束值大于总数据条数下标的值则赋值为总数据条数最大下标值
+                        end = json.length - 1;
+                    }
+                    json.forEach(function (item, i) {
+                        if (i >= start && i <= end) {
+                            item.checked = document.getElementById(item.nid).checked;
+                        }
+                    })
+                    pageThis--;
+                    console.log(pageThis)
+                    start = (pageThis - 1) * page_number;
                     end = start + page_number - 1;
                     if (end > json.length - 1) {
                         end = json.length - 1;
                     }
+                    clearTable();
+                    let tbody = document.querySelector("tbody");
+                    json.forEach(function (item, i) {
+
+                        if (i >= start && i <= end) {
+                            let tr = document.createElement("tr");
+                            if(item.checked===true){
+                                tr.innerHTML = `
+                            <td>${item.nid}</td>
+                            <td>${item.name}</td>
+                            <td>${item.time}</td>
+                            <td><input id="${item.nid}" name="${item.nid}"  type="checkbox" checked="${item.checked}" style="margin-left: 40px"></td>
+                            `
+                            }else {
+                                tr.innerHTML = `
+                            <td>${item.nid}</td>
+                            <td>${item.name}</td>
+                            <td>${item.time}</td>
+                            <td><input id="${item.nid}" name="${item.nid}"  type="checkbox" style="margin-left: 40px"></td>
+                            `
+                            }
+                            tbody.appendChild(tr);
+                            console.log(item.checked);
+                        }
+                    })
                 } else {
-                    start = 0;
-                    end = page_number - 1;
+                    alert("当前为第一页")
                 }
-                  json.forEach(function (item, i) {
+            }
+            next.onclick = function () {
+                if (pageThis < liAll.length - 2) { //当前页数小于最后一页则执行下一页
+                    if (pageThis > 1) {
+                        let start = (pageThis - 1) * page_number;
+                        let end = start + page_number - 1;
+                        if (end > json.length - 1) { //如果当页数据结束值大于总数据条数下标的值则赋值为总数据条数最大下标值
+                            end = json.length - 1;
+                        }
+                    } else {
+                        start = 0;
+                        end = page_number - 1;
+                    }
+                    json.forEach(function (item, i) {
                         if (i >= start && i <= end) {
 
                             item.checked = document.getElementById(item.nid).checked;
                         }
                     })
-
-                clearTable();
-                var tbody = document.querySelector("tbody");
-                json.forEach(function (item, i) {
-                    if (i >= start && i <= end) {
-                        var tr = document.createElement("tr");
-                        tr.innerHTML = `
-                             <td>${item.id}</td>
-                            <td>${item.name}</td>
-                            <td>${item.time}</td>
-                            <td><input id="${item.id}" name="${item.id}"  type="checkbox" style="margin-left: 40px"></td>
-                            `
-                        console.log(tr)
-                        tbody.appendChild(tr);
+                    pageThis++;
+                    console.log(pageThis);
+                    for (let j = 1; j < liAll.length - 1; j++) {
                     }
-                })
-            }
-        }
-
-        next.onclick = function () {
-            // alert(pagethis)
-            if (pagethis < liAll.length - 2) { //当前页数小于最后一页则执行下一页
-                pagethis++;
-                for (var j = 1; j < liAll.length - 1; j++) {
-
-                }
-
-                let start;
-                let end;
-                if (pagethis != 1) {
-                    start = (pagethis - 1) * page_number;
+                    start = (pageThis - 1) * page_number;
                     end = start + page_number - 1;
                     if (end > json.length - 1) {
                         end = json.length - 1;
                     }
-                } else {
-                    start = 0;
-                    end = page_number - 1;
-                }
-                json.forEach(function (item, i) {
+                    clearTable();
+                    let tbody = document.querySelector("tbody");
+                   json.forEach(function (item, i) {
+
                         if (i >= start && i <= end) {
-                            let index = document.getElementById(item.id).selectedIndex;
-                            item.value = document.getElementById(item.id).options[index].value;
+                            let tr = document.createElement("tr");
+                            if(item.checked===true){
+                                tr.innerHTML = `
+                            <td>${item.nid}</td>
+                            <td>${item.name}</td>
+                            <td>${item.time}</td>
+                            <td><input id="${item.nid}" name="${item.nid}"  type="checkbox" checked="${item.checked}" style="margin-left: 40px"></td>
+                            `
+                            }else {
+                                tr.innerHTML = `
+                            <td>${item.nid}</td>
+                            <td>${item.name}</td>
+                            <td>${item.time}</td>
+                            <td><input id="${item.nid}" name="${item.nid}"  type="checkbox" style="margin-left: 40px"></td>
+                            `
+                            }
+                            tbody.appendChild(tr);
+                            console.log(item.checked);
+                        }
+                    })
+                } else {
+                    alert("当前为最后一页")
+                }
+            }
+
+        },
+    error: function (xhr, textStatus, errorThrown) {
+            console.log("Error: " + errorThrown);
+            // 处理错误
+        }
+    });
+}
+function post(){ var data0=[];
+            if (pageThis > 1) {
+        start = (pageThis - 1) * page_number;
+        end = start + page_number - 1;
+        if (end > json.length - 1) { //如果当页数据结束值大于总数据条数下标的值则赋值为总数据条数最大下标值
+            end = json.length - 1;
+        }
+    } else {
+        start = 0;
+        end = page_number - 1;
+    }
+            json.forEach(function (item, i) {
+                        if (i >= start && i <= end) {
+                            item.checked = document.getElementById(item.nid).checked;
+                        }
+                        if(item.checked===true){
+                            data0.update(item.name)
                         }
                     })
 
-                clearTable();
-                var tbody = document.querySelector("tbody");
-                json.forEach(function (item, i) {
-                    if (i >= start && i <= end) {
-                        var tr = document.createElement("tr");
-                        tr.innerHTML = `
-                             <td>${item.id}</td>
-                            <td>${item.name}</td>
-                            <td>${item.time}</td>
-                            <td><input id="${item.id}" name="${item.id}"  type="checkbox" style="margin-left: 40px"></td>
-                            `
-                        console.log(tr)
-                        tbody.appendChild(tr);
-                    }
-                })
+
+
+           $.ajax({
+            url: '/merge/corpus_name/', //后端地址
+            type: 'POST',       //提交方式
+            data: data0
+            ,
+            dataType: "text",       //规定请求成功后返回的数据
+            success: function (data) {
+                //请求成功之后进入该方法，data为成功后返回的数据
+            },
+            error: function (errorMsg) {
+                //请求失败之后进入该方法，errorMsg为失败后返回的错误信息
             }
-        }
-            }
-        })
+        });
 
 
-    })
-
-
-
-
-
+     }
 
 
